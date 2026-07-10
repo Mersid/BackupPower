436420b644c3741206494971ff32b991495a1e18
Mersid <mersid_email@yahoo.com>
2026-07-10 11:45:48 -0700

    Moved files.

---
+++ Source/Source/BackupPower.cs
@@ -0,0 +1,20 @@
+using System;
+using JetBrains.Annotations;
+using UnityEngine;
+using Verse;
+
+namespace BackupPower;
+
+[UsedImplicitly] // By RimWorld
+public class BackupPower(ModContentPack content) : Mod(content)
+{
+    public static Settings Settings => LoadedModManager.GetMod<BackupPower>().GetSettings<Settings>();
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        Settings.DoWindowContents(inRect);
+    }
+
+    public override string SettingsCategory() => I18n.BackupPower;
+}
